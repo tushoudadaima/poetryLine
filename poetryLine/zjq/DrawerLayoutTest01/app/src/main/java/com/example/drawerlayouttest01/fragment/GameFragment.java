96571396875326f22ac75cd5fb1e8664033397e4
Jinhao01 <1459510381@qq.com>
2020-04-27 13:30:16 +0800

    jinhao

---
+++ poetryLine/zjq/DrawerLayoutTest01/app/src/main/java/com/example/drawerlayouttest01/fragment/GameFragment.java
@@ -0,0 +1,22 @@
+package com.example.drawerlayouttest01.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.drawerlayouttest01.R;
+
+public class GameFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.game_fragment,container,false);
+        return view;
+    }
+}
